b609c6bce59956a18c34551f301d7c7f026c01c0
Ashish <ashishalva1234@@gmail.com>
2025-09-01 18:48:54 +0530

    Add gunicorn to requirements

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v@!x$9)fk9ttdg$qp@ll+yqgrbsgv1c0(+4mh)%9*-@zh*oey6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ashishalva.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
